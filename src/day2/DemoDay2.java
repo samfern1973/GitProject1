ff9b2b0ead05c9c2dcb4c03a01f6821b61a98015
samfe <samfe@DESKTOP-1G29LAC.myfiosgateway.com>
2023-07-29 11:03:56 -0400

    Added DemoDay3 class

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -18,7 +18,7 @@ public class DemoDay2 {
 		//YASIR
 		public static void divide(int a, int b){
 			System.out.println(a/b);
-			
+		}	
 		//add method
 		public static void add(int a, int b, int c) {
 			System.out.println(a+b+c);

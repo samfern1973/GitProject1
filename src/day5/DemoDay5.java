09ff03abcb542eea595cf4e114b8e47185af5d0a
Sam <140651725+samfern1973@users.noreply.github.com>
2023-07-29 13:22:08 -0400

    Update DemoDay5.java

--- src/day5/DemoDay5.java
+++ src/day5/DemoDay5.java
@@ -5,6 +5,10 @@ public class DemoDay5 {
 	public static void main(String[] args) {
 		
 		System.out.println("HOT.. 90 degrees");
+
+	}
+	public void getData(){
+		System.out.println("But not as hot as Puerto Rico!");
 		
 	}
 

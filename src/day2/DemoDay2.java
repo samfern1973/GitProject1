c82c70f478c392e43011afee4360ddfdf9d826ba
samfe <samfe@DESKTOP-1G29LAC.myfiosgateway.com>
2023-07-29 10:34:14 -0400

    implemented add() & subtract()

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -6,5 +6,15 @@ public class DemoDay2 {
 		
 		System.out.println("Test");
 	}
+		//add method
+		public static void add(int a, int b) {
+			System.out.println(a+b);
+			
+		}
+		//subtract method
+		public static void subtract(int a, int b) {
+			System.out.println(a-b);
+		
+	}
 
 }
